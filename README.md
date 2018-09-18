d4519b421294046997bfb8f0bf1d699825ffb96d
Chris Fung <aergonaut@gmail.com>
2018-09-18 00:27:06 -0700

    implemented commit message checks

--- README.md
+++ README.md
@@ -3,18 +3,43 @@
 > A GitHub App built with [Probot](https://github.com/probot/probot) that
 > makes sure you've filled out all your paperwork correctly!
 
-## Setup
+## Usage
 
-```sh
-# Install dependencies
-npm install
+1.  [Install the application](https://github.com/app/secretary-bird) for your repositories or organizations.
+2.  Configure the application with a `.github/secretary-bird.yml` in your repository.
 
-# Run typescript
-npm run build
+## Configuration
 
-# Run the bot
-npm start
+```yaml
+pull_request_title:
+  pattern: .*
+  failure_message: >-
+    Markdown formatted string that will be included with failed check runs.
+  success_message: >-
+    Markdown formatted string that will be included with successful check runs.
+commit_title:
+  pattern: .*
+  failure_message: >-
+    Markdown formatted string that will be included with failed check runs.
+  success_message: >-
+    Markdown formatted string that will be included with successful check runs.
 ```
+
+### `pattern`
+
+Specify a regex pattern to match strings. By default this is `.*`.
+
+### `failure_message`
+
+Optional. This is a Markdown formatted string that will be included in the body
+of the check run on failure. Use this to include more information about your
+team's best practices.
+
+### `success_message`
+
+Optional. This is a Markdown formatted string that will be included in the body
+of the check run on success. Use this to provide some encouragement for a job
+well done.
 
 ## Contributing
 
--- src/checks.ts
+++ src/checks.ts
@@ -2,24 +2,39 @@ import { Context } from "probot";
 import { ChecksCreateParams } from "@octokit/rest";
 
 type Config = {
-  pull_request_title_regex: string;
-  commit_title_regex: string;
+  pull_request_title: {
+    pattern: string;
+    failure_message: string | null | undefined;
+    success_message: string | null | undefined;
+  };
+  commit_message: {
+    pattern: string;
+    failure_message: string | null | undefined;
+    success_message: string | null | undefined;
+  };
 };
 
 export async function run_checks(context: Context) {
   const config = await context.config("secretary-bird.yml", {
-    pull_request_title_regex: "^(CD|JZ)-\\d+\\s+.+",
-    commit_title_regex: ".*"
+    pull_request_title: {
+      pattern: ".*"
+    },
+    commit_message: {
+      pattern: ".*"
+    }
   });
 
-  check_title(context, config);
+  Promise.all([check_title(context, config), check_commits(context, config)]);
 }
 
 async function check_title(context: Context, config: Config) {
   context.log("checking title...");
+
   const pr = context.payload.pull_request;
   let title: string = pr.title;
-  let pull_request_title_regex = new RegExp(config.pull_request_title_regex);
+  const pull_request_title_regex = new RegExp(
+    config.pull_request_title.pattern
+  );
 
   let title_passes_check = pull_request_title_regex.test(title);
   let data: ChecksCreateParams;
@@ -30,13 +45,17 @@ async function check_title(context: Context, config: Config) {
       "The Pull Request title does not match the repository's configured PR title format."
     );
     summary.push(
-      `The required format is: \`${pull_request_title_regex}\`. Your title was "${title}"`
+      `The required format is: \`${pull_request_title_regex}\`. Your title was "${title}".`
     );
+    let failure_message = config.pull_request_title.failure_message;
+    if (failure_message) {
+      summary.push(failure_message);
+    }
 
     data = {
       owner: context.repo().owner,
       repo: context.repo().repo,
-      name: "Title Format",
+      name: "title-format",
       head_sha: pr.head.sha,
       status: "completed",
       conclusion: "action_required",
@@ -53,7 +72,7 @@ async function check_title(context: Context, config: Config) {
     data = {
       owner: context.repo().owner,
       repo: context.repo().repo,
-      name: "Title Format",
+      name: "title-format",
       head_sha: pr.head.sha,
       status: "completed",
       conclusion: "success",
@@ -61,6 +80,74 @@ async function check_title(context: Context, config: Config) {
       output: {
         title: "Title format",
         summary: summary.join("\n\n")
+      }
+    };
+  }
+
+  context.github.checks.create(data);
+}
+
+type CommitInfo = {
+  sha: string;
+  message: string;
+};
+
+async function check_commits(context: Context, config: Config) {
+  context.log("checking commits...");
+
+  const pr = context.payload.pull_request;
+
+  const compare = await context.github.repos.compareCommits(
+    context.repo({
+      base: pr.base.sha,
+      head: pr.head.sha
+    })
+  );
+  const commits: [CommitInfo] = compare.data.commits;
+
+  const commit_message_regex = new RegExp(config.commit_message.pattern);
+  const pattern_failed_commits = commits.filter(
+    commit => !commit_message_regex.test(commit.message)
+  );
+
+  let data: ChecksCreateParams;
+  if (pattern_failed_commits.length) {
+    let summary: string[] = [];
+    summary.push(
+      "One or more commits in this Pull Request do not follow the repository's configured commit message format.\n"
+    );
+    summary.push(`The required format is: \`${commit_message_regex}\`.\n`);
+    pattern_failed_commits.forEach(commit => {
+      summary.push(`\`${commit.sha}\` ${commit.message}`);
+    });
+
+    data = {
+      owner: context.repo().owner,
+      repo: context.repo().repo,
+      name: "commit-msg",
+      head_sha: pr.head.sha,
+      status: "completed",
+      conclusion: "action_required",
+      completed_at: new Date().toISOString(),
+      output: {
+        title: "Commit Messages",
+        summary: summary.join("\n")
+      }
+    };
+  } else {
+    let summary = "All good!\n\n";
+
+    data = {
+      owner: context.repo().owner,
+      repo: context.repo().repo,
+      name: "commit-msg",
+      head_sha: pr.head.sha,
+      status: "completed",
+      conclusion: "success",
+      completed_at: new Date().toISOString(),
+      output: {
+        title: "Commit Messages",
+        summary
       }
     };
   }

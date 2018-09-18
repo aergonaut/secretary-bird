098d17742390ae95e19899ff9ec7504342fbb3d7
Chris Fung <aergonaut@gmail.com>
2018-09-17 18:40:38 -0700

    add successful branch

--- src/checks.ts
+++ src/checks.ts
@@ -2,13 +2,13 @@ import { Context } from "probot";
 import { ChecksCreateParams } from "@octokit/rest";
 
 type Config = {
-  pull_request_title_regex: string,
-  commit_title_regex: string
-}
+  pull_request_title_regex: string;
+  commit_title_regex: string;
+};
 
 export async function run_checks(context: Context) {
   const config = await context.config("secretary-bird.yml", {
-    pull_request_title_regex: "^(CD|JZ)-\d+\s+.+",
+    pull_request_title_regex: "^(CD|JZ)-\\d+\\s+.+",
     commit_title_regex: ".*"
   });
 
@@ -22,24 +22,48 @@ async function check_title(context: Context, config: Config) {
   let pull_request_title_regex = new RegExp(config.pull_request_title_regex);
 
   let title_passes_check = pull_request_title_regex.test(title);
-  if (!title_passes_check) {
-    let summary = []
-    summary.push("The Pull Request title does not match the repository's configured PR title format.");
-    summary.push(`The required format is: \`${pull_request_title_regex}\``);
+  let data: ChecksCreateParams;
 
-    let data: ChecksCreateParams = {
+  if (!title_passes_check) {
+    let summary = [];
+    summary.push(
+      "The Pull Request title does not match the repository's configured PR title format."
+    );
+    summary.push(
+      `The required format is: \`${pull_request_title_regex}\`. Your title was "${title}"`
+    );
+
+    data = {
       owner: context.repo().owner,
       repo: context.repo().repo,
-      name: "Secretary Bird",
+      name: "Title Format",
       head_sha: pr.head.sha,
       status: "completed",
       conclusion: "action_required",
-      completed_at: (new Date()).toISOString(),
+      completed_at: new Date().toISOString(),
       output: {
         title: "Title format",
         summary: summary.join("\n\n")
       }
-    }
+    };
+  } else {
+    let summary = [];
+    summary.push("All good!");
+
+    data = {
+      owner: context.repo().owner,
+      repo: context.repo().repo,
+      name: "Title Format",
+      head_sha: pr.head.sha,
+      status: "completed",
+      conclusion: "success",
+      completed_at: new Date().toISOString(),
+      output: {
+        title: "Title format",
+        summary: summary.join("\n\n")
+      }
+    };
   }
-  context.github.checks.create(data)
+
+  context.github.checks.create(data);
 }
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import { Application } from 'probot'
+import { Application } from "probot";
 
 import { run_checks } from "./checks";
 
 export = (app: Application) => {
   // Your code here
-  app.log('Yay, the app was loaded!')
+  app.log("Yay, the app was loaded!");
 
   app.on(["pull_request.opened", "pull_request.synchronize"], run_checks);
-}
+};
